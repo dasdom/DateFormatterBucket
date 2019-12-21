909acefb2031573d3691b47fd8a817d2892d0670
Dominik Hauser <dom@dasdom.de>
2019-12-21 07:29:55 +0100

    Add formatter method

--- Sources/DateFormatterBucket/DateFormatterBucket.swift
+++ Sources/DateFormatterBucket/DateFormatterBucket.swift
@@ -3,20 +3,33 @@ import Foundation
 
 public class DateFormatterBucket {
   
-    public static let shared = DateFormatterBucket()
+  public static let shared = DateFormatterBucket()
   
-    var dateFormatters: [String: DateFormatter] = [:]
+  var dateFormatters: [String: DateFormatter] = [:]
   
-    public subscript(format: String) -> DateFormatter {
-        get {
-            if let formatter = dateFormatters[format] {
-                return formatter
-            } else {
-                let formatter = DateFormatter()
-                formatter.dateFormat = format
-                dateFormatters[format] = formatter
-                return formatter
-            }
-        }
+  public func formatter(dateStyle: DateFormatter.Style, timeStyle: DateFormatter.Style) -> DateFormatter {
+    let key = "\(dateStyle),\(timeStyle)"
+    if let formatter = dateFormatters[key] {
+      return formatter
+    } else {
+      let formatter = DateFormatter()
+      formatter.dateStyle = dateStyle
+      formatter.timeStyle = timeStyle
+      dateFormatters[key] = formatter
+      return formatter
     }
+  }
+  
+  public subscript(format: String) -> DateFormatter {
+    get {
+      if let formatter = dateFormatters[format] {
+        return formatter
+      } else {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        dateFormatters[format] = formatter
+        return formatter
+      }
+    }
+  }
 }
--- Tests/DateFormatterBucketTests/DateFormatterBucketTests.swift
+++ Tests/DateFormatterBucketTests/DateFormatterBucketTests.swift
@@ -3,6 +3,10 @@ import XCTest
 
 final class DateFormatterBucketTests: XCTestCase {
   
+  override func tearDown() {
+    DateFormatterBucket.shared.dateFormatters.removeAll()
+  }
+  
   func test_returnsPreviousFormatter_forSameFormat() {
     
     let dateFormatter = DateFormatterBucket.shared["yyyy-MM-dd"]
@@ -10,5 +14,19 @@ final class DateFormatterBucketTests: XCTestCase {
     
     XCTAssertEqual(dateFormatter, newDateFormatter)
     XCTAssertEqual(1, DateFormatterBucket.shared.dateFormatters.count)
+  }
+  
+  func test_subscriptSetsFormat() {
+    let format = "yyyy-MM-dd HH:ss"
+    let dateFormatter = DateFormatterBucket.shared[format]
+    
+    XCTAssertEqual(format, dateFormatter.dateFormat)
+  }
+  
+  func test_formatter_setsTimeStyleAndDateStyle() {
+    let dateFormatter = DateFormatterBucket.shared.formatter(dateStyle: .long, timeStyle: .short)
+    
+    XCTAssertEqual(dateFormatter.dateStyle, .long)
+    XCTAssertEqual(dateFormatter.timeStyle, .short)
   }
 }
